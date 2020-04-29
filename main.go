272e5f9048fed07770ac4cadc64cada66ca111fd
Daniel Xu <xuyiwei1998@gmail.com>
2020-04-28 21:42:59 -0400

    Add messaging to telegram bot

--- main.go
+++ main.go
@@ -3,13 +3,49 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"regexp"
 	"strings"
 	"time"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 func main() {
+
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
+	if err != nil {
+		panic(err) // You should add better error handling than this!
+	}
+
+	bot.Debug = true // Has the library display every request and response.
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	var botChatID int64
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		botChatID = update.Message.Chat.ID
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "successfully linked")
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+		break
+	}
 
 	for {
 		resp, err := http.Get("http://www.adm.uwaterloo.ca/cgi-bin/cgiwrap/infocour/salook.pl?level=under&sess=1205&subject=CS&cournum=486")
@@ -38,9 +74,15 @@ func main() {
 			re := regexp.MustCompile("[0-9]+")
 			numSlice := re.FindAllString(val, -1)
 			if numSlice[4] < numSlice[5] {
-				fmt.Printf("Seat available at section %d\n", index)
+				message := fmt.Sprintf("Seat available at section %d\n", index)
+				fmt.Printf(message)
+				msg := tgbotapi.NewMessage(botChatID, message)
+				bot.Send(msg)
 			} else {
-				fmt.Printf("No available seat at section %d\n", index)
+				message := fmt.Sprintf("No available seat at section %d\n", index)
+				fmt.Printf(message)
+				msg := tgbotapi.NewMessage(botChatID, message)
+				bot.Send(msg)
 			}
 		}
 
